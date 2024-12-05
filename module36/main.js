8a803f830eb6d5d40001b189e84a018f91d56aba
olsaa0 <olsaolsa30@gmail.com>
2024-12-05 16:08:52 +0100

    jquery animation

--- module36/main.js
+++ module36/main.js
@@ -9,3 +9,20 @@ $('#btn').click(function(){
 $('#div1').click(function(){
     $(this).addClass('clicked');
 }).find('span').attr('title', 'this is a titlee');
+
+$('#btnShow').click(function(){
+    $('.hidden').show('slow');
+})
+$('#btnHide').click(function(){
+    $('.hidden').hide();
+})
+
+$('.animate').click(function(){
+    $(this).animate({
+        'left': '+=30px',
+        'width': '300px',
+        'height': '300px',
+        'font-size': '50px',
+        'opacity': '-=0.1',
+    })
+})
